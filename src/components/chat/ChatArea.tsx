e7a0704d9d836a4605d0304351a9fd1a60715ebf
qiy63 <qiy63.biz@gmail.com>
2026-02-26 22:03:21 +0800

    deploy2.4

--- src/components/chat/ChatArea.tsx
+++ src/components/chat/ChatArea.tsx
@@ -27,7 +27,7 @@ export const ChatArea: React.FC<ChatAreaProps> = ({
   onNewChat,
 }) => {
   return (
-    <div className="flex-1 flex flex-col h-screen">
+    <div className="flex-1 min-h-0 flex flex-col">
       <ChatTabs
         tabs={chatTabs}
         activeId={activeChatId}
@@ -38,7 +38,7 @@ export const ChatArea: React.FC<ChatAreaProps> = ({
       {messages.length === 0 ? (
         <EmptyState onPromptClick={onSendMessage} />
       ) : (
-        <div className="flex-1 overflow-y-auto px-6 py-8 space-y-6">
+        <div className="flex-1 min-h-0 overflow-y-auto px-6 py-8 space-y-6">
           {messages.map((msg) => (
             <ChatMessage key={msg.id} message={msg} />
           ))}
